0aa87baea218bd89ba0d036e03d667f6f8b1f52c
Bora Yılmazmış <borayilmazmis@hotmail.com>
2023-04-14 19:55:37 +0300

    yazilar modeli oluşturuldu

---
+++ blog/models/yazi.py
@@ -0,0 +1,19 @@
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import KategoriModel #kategoriler attribute ü için
+from django.contrib.auth.models import User #yazar attribute ü için
+
+class YazilarModel(models.Model):
+    resim = models.ImageField(upload_to='yazi_resimleri')
+    baslik = models.CharField(max_length=50)
+    icerik = models.TextField()
+    oluşturulma_tarihi = models.DateTimeField(auto_now_add=True) #Kayıt oluşturulduğu anda oluşturulan tarih oto kaydedilir
+    duzenleme_tarihi = models.DateTimeField(auto_now=True) #Kayıt düzenlendiğinde oto olarak düzenlenme tarihi alanı değişir
+    slug = AutoSlugField(populate_from = 'baslik', unique=True) #www.oguzhancelikarslan.com/yazilar/pguzhan-celikarslan-kimdir? diye baslik olusturmamızı sağlar
+    kategoriler = models.ManyToManyField(KategoriModel, related_name='yazi') #Bir yazının birden fazla kategoriye atanmasını sağlar
+    yazar = models.ForeignKey(User, on_delete=models.CASCADE, related_name='yazilar')#on_delete-> yazar silinirse bütün yazilarini siler
+
+    class Meta:
+        verbose_name = 'Yazi'
+        verbose_name_plural = 'Yazilar'
+        db_name = 'Yazi'
